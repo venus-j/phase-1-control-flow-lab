f435f888c7271dbe7773bb153ea4a665f138aa59
venus-j <chumbavenus24@gmail.com>
2024-07-10 12:44:14 +0300

    control-flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// Write your code in here!
+
+// Define the scuberGreetingForFeet function
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if (distance > 400 && distance <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (distance > 2000 && distance <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// Define the ternaryCheckCity function
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// Define the switchOnCharmFromTip function
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
+
+
